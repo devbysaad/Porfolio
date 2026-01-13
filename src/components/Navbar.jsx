54ce2217ad1d102df1cadd5bf79c67777603e492
devbysaad <dev.bysaad@gmail.com>
2026-01-14 03:04:49 +0500

    refactor(Navbar): remove commented logo image from navbar
    The logo image was commented out as it's no longer needed in the current design. This change simplifies the navbar component by removing unused code.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <header className='header'>
       <NavLink to='/'>
-        <img src="/src/assets/devbysaad.png" alt="" className="w-16 h-16"/>
+        {/* <img src="/src/assets/devbysaad.png" alt="" className="w-16 h-16"/> */}
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-white" }>
